702f32ef4f4b9934a97c31a08e46aeb0bcd8e0b7
lsy <793355419@qq.com>
2023-07-25 15:24:41 +0800

    ocr识别

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,104 +1,68 @@
 <template>
   <div class="publish">
-    <video ref="video"></video>
-    <canvas style="display: none" id="canvasCamera"></canvas>
-    <div v-if="imgSrc" class="img_bg_camera">
-      <img :src="imgSrc" class="tx_img" />
-    </div>
-    <button @click="OpenCamera">打开摄像头</button>
-    <button @click="CloseCamera">关闭摄像头</button>
-    <button @click="setImage">拍照</button>
+    <button @click="openMedia">123456789</button>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      mediaStreamTrack: {},
-      video_stream: '', // 视频stream
-      imgSrc: '', // 拍照图片
-      canvas: null,
-      context: null
-    }
-  },
-  mounted () {
-    // 进入页面 自动调用摄像头
-    this.getCamera()
+  created () {
+    this.init()
   },
   methods: {
-    // 调用打开摄像头功能
-    getCamera () {
-      // document.querySelector('video').play()
-      // 获取 canvas 画布
-      this.canvas = document.getElementById('canvasCamera')
-      this.context = this.canvas.getContext('2d')
-      // 旧版本浏览器可能根本不支持mediaDevices，我们首先设置一个空对象
+    // 正式启动摄像头
+    openMedia () {
+      // 摄像头调用配置
+      const mediaOpts = {
+        audio: false,
+        video: {
+          width: 1280,
+          aspectRatio: { ideal: 16 / 9 }, // 设置期望的宽高比
+          frameRate: { ideal: 30 }, // 设置期望的帧率
+          facingMode: { exact: 'environment' }
+        }
+        // video: { facingMode: 'environment' } // 调用前置摄像头
+        // video: { width: 1280, height: 720 },  //影响呈现的大小。注意不能用百分比
+        //  video: { facingMode: { exact: "environment" }{ facingMode: { exact: "environment" } }// 这个是调用后置摄像头
+      }
+
+      navigator.mediaDevices
+        .getUserMedia(mediaOpts)
+        .then((stream) => {
+          this.$router.push({
+            path: '/',
+            query: {
+              stream
+            }
+          })
+        })
+        .catch((e) => {
+          alert(e)
+        })
+    },
+    init () {
+      window.URL =
+        window.URL || window.webkitURL || window.mozURL || window.msURL
       if (navigator.mediaDevices === undefined) {
         navigator.mediaDevices = {}
       }
-      // 正常支持版本
-      navigator.mediaDevices
-        .getUserMedia({
-          video: { facingMode: { exact: 'environment' } }
-        })
-        .then((stream) => {
-          // 摄像头开启成功
-          this.mediaStreamTrack =
-            typeof stream.stop === 'function' ? stream : stream.getTracks()[0]
-          this.video_stream = stream
-          this.$refs.video.srcObject = stream
-          this.$refs.video.play()
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    },
-    // 拍照 绘制图片
-    setImage () {
-      console.log('拍照')
-      // 点击canvas画图
-      this.context.drawImage(this.$refs.video, 0, 0, 200, 100)
-      // 获取图片base64链接
-      const image = this.cancas.toDataURL('image/png')
-      this.imgSrc = image
-      this.$emit('refreshDataList', this.imgSrc)
-    },
-    // 打开摄像头
-    OpenCamera () {
-      console.log('打开摄像头')
-      this.getCamera()
-    },
-    // 关闭摄像头
-    CloseCamera () {
-      console.log('关闭摄像头')
-      this.$refs.video.srcObject.getTracks()[0].stop()
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
+          const getUserMedia =
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia
+          if (!getUserMedia) {
+            return Promise.reject(
+              new Error('getUserMedia is not implemented in this browser')
+            )
+          }
+          return new Promise(function (resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject)
+          })
+        }
+      }
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-video {
-  width: 100%;
-  height: 300px;
-}
-canvas {
-  width: 100%;
-  height: 300px;
-}
-button {
-  width: 100px;
-  height: 40px;
-  position: relative;
-  bottom: 0;
-  left: 0;
-  background-color: rgb(22, 204, 195);
-}
-.img_bg_camera {
-  img {
-    width: 300px;
-    height: 200px;
-  }
-}
-</style>
--- src/views/扫描备用.vue
+++ src/views/扫描备用.vue
@@ -12,24 +12,25 @@
         <div class="left"></div>
         <div class="cc" id="idCardImg">
           <img class="idImg" src="@/assets/idBorder.png" alt="" />
-          <!-- 摄像展示要用video标签 -->
-          <video
-            autoplay
-            id="video"
-            ref="videoDom"
-            class="bg"
-            muted
-            controlsList="nodownload"
-            disablePictureInPicture
-            webkit-playsinline="true"
-            playsinline="true"
-          ></video>
         </div>
         <div class="right"></div>
       </div>
 
       <div class="down"></div>
     </div>
+
+    <!-- 摄像展示要用video标签 -->
+    <video
+      autoplay
+      id="video"
+      ref="videoDom"
+      class="bg"
+      muted
+      controlsList="nodownload"
+      disablePictureInPicture
+      webkit-playsinline="true"
+      playsinline="true"
+    ></video>
 
     <!-- 截取的照片用canvas标签展示 -->
 
@@ -38,6 +39,9 @@
 </template>
 
 <script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import html2canvas from 'html2canvas'
 export default {
   name: 'HomeView',
   data () {
@@ -50,10 +54,10 @@ export default {
     }
   },
   created () {
-    this.successFun(this.$route.query.stream)
+    this.init()
   },
   mounted () {
-    this.openMedia()
+    // this.openMedia()
   },
   methods: {
     init () {
@@ -88,13 +92,15 @@ export default {
         audio: false,
         video: {
           width: 1280,
+          height: 720,
           aspectRatio: { ideal: 16 / 9 }, // 设置期望的宽高比
-          frameRate: { ideal: 30 }, // 设置期望的帧率
+          frameRate: { ideal: 60 }, // 设置期望的帧率
+          // height: 800,
           facingMode: { exact: 'environment' }
         }
         // video: { facingMode: 'environment' } // 调用前置摄像头
         // video: { width: 1280, height: 720 },  //影响呈现的大小。注意不能用百分比
-        //  video: { facingMode: { exact: "environment" }{ facingMode: { exact: "environment" } }// 这个是调用后置摄像头
+        //  video: { facingMode: { exact: "environment" } }// 这个是调用后置摄像头
       }
 
       console.log(this.video)
@@ -148,16 +154,37 @@ export default {
 
     // 截取视频
     drawMedia () {
+      // html2canvas(document.getElementById('qr-canvas')).then(function (canvas) {
+      //   console.log(canvas)
+      //   const img = new Image()
+      //   img.src = canvas.toDataURL('image/jpeg')
+      //   img.style.cssText += 'position:absolute;width:100%;left:0;top:0;'
+      //   document.getElementById('id').appendChild(img)
+
+      //   // img.style.cssText += 'position:absolute;width:100%;left:0;top:0;'
+      // })
       const canvas = this.$refs.canvasRef
       const video = this.$refs.videoDom
+      canvas.width = video.clientWidth
+      canvas.height = video.clientHeight
+      console.log(canvas, '截取', video)
       const ctx = canvas.getContext('2d')
-      canvas.width = 600
-      canvas.height = 800
+      console.log(ctx, 'ctx')
       ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
       const img = new Image()
       img.src = canvas.toDataURL('image/png')
       document.getElementById('id').appendChild(img)
       this.downFile(img.src, '截图.jpg')
+      // this.content1 = this.canvas1.getContext('2d')
+      // console.log(this.canvas1, '画布')
+
+      // this.context1.drawImage(
+      //   this.video,
+      //   0,
+      //   0,
+      //   this.video.videoWidth,
+      //   this.video.videoHeight
+      // )
     },
     downFile (data, fileName) {
       const link = document.createElement('a')
@@ -165,6 +192,7 @@ export default {
       link.href = data
       link.download = fileName
       link.click()
+
       // 释放内存
       window.URL.revokeObjectURL(link.href)
     }
@@ -192,47 +220,43 @@ video::-webkit-media-controls-enclosure {
   bottom: 0;
   right: 0;
   .mask {
+    // overflow: hidden;
     position: relative;
     z-index: 99999;
     display: flex;
     flex-direction: column;
     .top {
       height: 30vh;
-      background-color: rgba(0, 0, 0, 1);
+      background-color: rgba(0, 0, 0, 0.5);
     }
     .center {
       display: flex;
-      height: 200px;
+      height: 218px;
       .left,
       .right {
         width: 15px;
         height: 100%;
-        position: relative;
-        z-index: 0;
-        background-color: rgba(0, 0, 0, 1);
+        background-color: rgba(0, 0, 0, 0.5);
       }
       .cc {
-        position: relative;
         flex: 1;
         .idImg {
           width: 100%;
-          height: 200px;
+          height: 218px;
         }
       }
     }
     .down {
-      background-color: rgba(0, 0, 0, 1);
+      background-color: rgba(0, 0, 0, 0.5);
       height: 46vh;
     }
   }
   .bg {
-    width: 100%;
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: -1;
   }
 }
 </style>
